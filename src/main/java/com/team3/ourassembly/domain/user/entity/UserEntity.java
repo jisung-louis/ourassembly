2be85047c9226d8687243581804f9f3c04c53a6c
jisung-louis <khkh9357@naver.com>
2026-03-23 15:47:40 +0900

    feat : User Entity 구현

---
+++ src/main/java/com/team3/ourassembly/domain/user/entity/UserEntity.java
@@ -0,0 +1,31 @@
+package com.team3.ourassembly.domain.user.entity;
+
+import com.team3.ourassembly.global.BaseTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "user")
+public class UserEntity extends BaseTime {
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String address;
+}
