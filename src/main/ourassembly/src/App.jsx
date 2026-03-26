3dfcf43ab7689ca5c19fa796102ea014f790df62
jisung-louis <khkh9357@naver.com>
2026-03-26 16:11:05 +0900

    feat : 페이지/컴포넌트 이름 변경

--- src/main/ourassembly/src/App.jsx
+++ src/main/ourassembly/src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { HomePage } from './pages/HomePage.jsx'
-import { MemberBoardPage } from './pages/MemberBoardPage.jsx'
-import { MemberDetailPage } from './pages/MemberDetailPage.jsx'
+import { CongressOpinionPage } from './pages/CongressOpinionPage.jsx'
+import { CongressDetailPage } from './pages/CongressDetailPage.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<HomePage />} path="/" />
-        <Route element={<MemberDetailPage />} path="/members/:memberId" />
-        <Route element={<MemberBoardPage />} path="/members/:memberId/board" />
+        <Route element={<CongressDetailPage />} path="/members/:memberId" />
+        <Route element={<CongressOpinionPage />} path="/members/:memberId/board" />
         <Route element={<Navigate replace to="/" />} path="*" />
       </Routes>
     </BrowserRouter>
