e90a4ecea6ce983c8bd0e69852443407bae6b237
ys06o <yongseongg00@gmail.com>
2026-03-26 14:24:59 +0900

    fix:opinion crud 리팩토링

--- src/main/java/com/team3/ourassembly/domain/opinion/controller/OpinionController.java
+++ src/main/java/com/team3/ourassembly/domain/opinion/controller/OpinionController.java
@@ -4,6 +4,7 @@ import com.team3.ourassembly.domain.opinion.dto.opinion.OpinionCreateRequestDto;
 import com.team3.ourassembly.domain.opinion.dto.opinion.OpinionResponseDto;
 import com.team3.ourassembly.domain.opinion.service.OpinionService;
 import com.team3.ourassembly.domain.opinion.dto.opinion.OpinionUpdateRequestDto;
+import com.team3.ourassembly.domain.user.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +17,7 @@ import java.util.List;
 @RequestMapping("/opinion")
 public class OpinionController {
     private final OpinionService opinionService;
-
+    private final JwtService jwtService;
 
 
 
@@ -24,26 +25,22 @@ public class OpinionController {
     @PostMapping
     public ResponseEntity<OpinionResponseDto> createOpinion(@RequestBody OpinionCreateRequestDto requestDto
             ,@RequestHeader("Authorization") String token){
+        // 1. 토큰 확인
+        if (token == null || !token.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
 
+        // 2. 순수 토큰 추출
+        String pureToken = token.replace("Bearer ", "");
 
-        //TODO:토큰 기능 구현하면 주석풀기
-//        // 1. 토큰 확인
-//        if (token == null || !token.startsWith("Bearer ")) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        }
-//
-//        // 2. 순수 토큰 추출
-//        String pureToken = token.replace("Bearer ", "");
-//
-//        // 3. 아이디 추출
-//        String loginMid = jwtService.getClaim(pureToken);
-//        if (loginMid == null) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        }
+        // 3. 아이디 추출
+        String loginId = jwtService.getClaim(pureToken);
+        if (loginId == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
 
         // 4. 서비스 호출
-        String loginMid="testUser"; //임시 테스트
-        OpinionResponseDto response = opinionService.create(requestDto, loginMid);
+        OpinionResponseDto response = opinionService.create(requestDto, loginId);
 
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -51,26 +48,46 @@ public class OpinionController {
     } //method end
 
 
-    //특정국회의원 의견게시판 목록 조회:R
-    @GetMapping("/opinions")
-    public ResponseEntity<List<OpinionResponseDto>> getOpinions(@RequestParam Integer congress_id) {
-
-        List<OpinionResponseDto> response=opinionService.getOpinions(congress_id);
-        return ResponseEntity.ok(response);
-    } //method end
-
 
     //의견 수정:U
     @PutMapping("/opinion")
-    public ResponseEntity<OpinionResponseDto> putOpinion(@RequestParam Long opinion_id, @RequestBody OpinionUpdateRequestDto requestDto) {
-        OpinionResponseDto responseDto=opinionService.update(opinion_id,requestDto);
+    public ResponseEntity<OpinionResponseDto> putOpinion(@RequestParam Long opinion_id,@RequestBody OpinionUpdateRequestDto requestDto
+    ,String token) {
+        // 1. 토큰 확인
+        if (token == null || !token.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        // 2. 순수 토큰 추출
+        String pureToken = token.replace("Bearer ", "");
+
+        // 3. 아이디 추출
+        String loginId = jwtService.getClaim(pureToken);
+        if (loginId == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        OpinionResponseDto responseDto=opinionService.update(opinion_id,requestDto,loginId);
         return ResponseEntity.ok(responseDto);
     }
 
     //의견 삭제:D
     @DeleteMapping
-    public ResponseEntity<String> delete(@RequestParam Long opinion_id) {
-        boolean isDeleted = opinionService.deleteOpinion(opinion_id);
+    public ResponseEntity<String> delete(@RequestParam Long opinion_id,String token) {
+        // 1. 토큰 확인
+        if (token == null || !token.startsWith("Bearer ")) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        // 2. 순수 토큰 추출
+        String pureToken = token.replace("Bearer ", "");
+
+        // 3. 아이디 추출
+        String loginId = jwtService.getClaim(pureToken);
+        if (loginId == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        boolean isDeleted = opinionService.delete(opinion_id,loginId);
 
         if (isDeleted) {
             return ResponseEntity.ok("삭제 성공!");
@@ -78,8 +95,17 @@ public class OpinionController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body("삭제 실패: 게시글을 찾을 수 없습니다.");
         }
-    }
+    } //method end
 
 
+
+
+    //특정국회의원 의견게시판 목록 조회:R
+    @GetMapping("/opinions")
+    public ResponseEntity<List<OpinionResponseDto>> getOpinions(@RequestParam Long id) {
+
+        List<OpinionResponseDto> response=opinionService.getOpinions(id);
+        return ResponseEntity.ok(response);
+    } //method end
 
 } //class end
