53a3c45e8d4439470cec7b47aa810cc2b089603f
jisung-louis <khkh9357@naver.com>
2026-03-26 11:56:12 +0900

    feat : 지역구로 국회의원 검색 반환용 DTO 구현

---
+++ src/main/java/com/team3/ourassembly/domain/congress/dto/DistrictResponse.java
@@ -0,0 +1,24 @@
+package com.team3.ourassembly.domain.congress.dto;
+
+import com.team3.ourassembly.domain.congress.entity.CongressmanEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class DistrictResponse{
+    private Long id;
+    private String address1; // 시/도
+    private String address2; // 시/군/구
+    private String address3; // 읍/면/동
+    private Long congressmanId;
+    private String congressmanName;
+    private String congressmanPhotoUrl;
+    private String congressmanParty;
+    private String congressmanWard;
+}
