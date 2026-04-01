d2940780ab744c5fad0cb2ef13a1cd49117a810f
jisung-louis <khkh9357@naver.com>
2026-04-01 10:43:34 +0900

    chore : 초기 데이터 SQL 쿼리 세팅

---
+++ src/main/resources/sql/01_adminSet.sql
@@ -0,0 +1,2 @@
+INSERT INTO ourassembly.user (id, created_at, updated_at, address, email, name, password) VALUES (1, '2026-04-01 00:00:00.000000', '2026-04-01 00:00:00.000000', null, 'admin@gmail.com', '관리자', '$2a$10$J2V9dUkkHbRWl1IWk8NAFuYd8aoYd3hs4FVUWyuD02.t8WSu9pyva');
+INSERT INTO ourassembly.user (id, created_at, updated_at, address, email, name, password) VALUES (2, '2026-04-01 00:00:00.000000', '2026-04-01 00:00:00.000000', null, 'congress@gmail.com', '테스트국회의원계정', '$2a$10$TyisN7dO98kCPFe.Vr2HKumGP2hwWJyUrEFiLyOpPx5wfGjpI5Uji');
