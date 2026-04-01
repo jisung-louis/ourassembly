f308b8fc15ca544790e3182fd36f0f34d5cd076f
jisung-louis <khkh9357@naver.com>
2026-04-01 11:53:50 +0900

    chore : 환경변수 예시 파일 추가, BaseUrl 포트 8080으로 수정

--- src/main/ourassembly/src/services/apiClient.js
+++ src/main/ourassembly/src/services/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const DEFAULT_API_BASE_URL = 'http://localhost:8082'
+const DEFAULT_API_BASE_URL = 'http://localhost:8080'
 
 function getApiBaseUrl() {
   const configuredBaseUrl = import.meta.env.VITE_API_BASE_URL ?? DEFAULT_API_BASE_URL
