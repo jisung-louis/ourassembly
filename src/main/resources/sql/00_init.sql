34a2c5293b2f8ce0175a0d7387c544f17b140db5
jisung-louis <khkh9357@naver.com>
2026-04-03 15:45:22 +0900

    chore : 국회의원 초기 데이터 시드 SQL을 재구성

---
+++ src/main/resources/sql/00_init.sql
@@ -0,0 +1,8 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+DELETE FROM ourassembly.district;
+DELETE FROM ourassembly.congressman_committee;
+DELETE FROM ourassembly.committee;
+DELETE FROM ourassembly.congressman;
+
+SET FOREIGN_KEY_CHECKS = 1;
